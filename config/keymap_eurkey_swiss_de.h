454919ab098ae95a87f70f06493cec5f1f397409
Bubble <36994260+bubbleology@users.noreply.github.com>
2023-08-14 19:23:06 +0200

    update eurkey

--- config/keymap_eurkey_swiss_de.h
+++ config/keymap_eurkey_swiss_de.h
@@ -2,13 +2,14 @@
  * This file is used for custom keycodes to get EURKEY working on US Int language in OS
 **/
 
-// EURkey
-#define CH_Z    KC_Y    // Y
-#define CH_Y    KC_Z    // Z
-#define CH_UDIA ALGR(CH_U) // ü
-#define CH_ODIA ALGR(CH_O) // ö
-#define CH_ADIA ALGR(CH_A) // ä
-#define CH_EGRV ALGR(KC_F) // è
-#define CH_EACU ALGR(KC_G) // é
-#define CH_AGRV ALGR(KC_Z) // à
-#define CH_AACU ALGR(KC_X) // á
+// Base
+#define CH_Z    CH_Y    // Y
+#define CH_Y    CH_Z    // Z
+// AltGr Symbols
+#define CH_UDIA ALGR(U) // ü
+#define CH_ODIA ALGR(O) // ö
+#define CH_ADIA ALGR(A) // ä
+#define CH_EGRV ALGR(F) // è
+#define CH_EACU ALGR(G) // é
+#define CH_AGRV ALGR(Z) // à
+#define CH_AACU ALGR(X) // á
